b1b2089dca9cb9532a19bbf53e9d08aed0474962
nuliows <nuliows@gmail.com>
2023-06-08 18:29:50 +0300

    add spring-demo project

--- Spring-education/spring-di-firstLesson/src/main/java/eu/codeacademy/config/SpringContextConfig.java
+++ Spring-education/spring-di-firstLesson/src/main/java/eu/codeacademy/config/SpringContextConfig.java
@@ -1,9 +1,17 @@
 package eu.codeacademy.config;
 
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({"eu.codeacademy.*"})
 public class SpringContextConfig {
+
+    //jeigu nededam anotacijos ant klases InternalMarksDao, galime pasirasyti tokia israiska
+//    @Bean
+//    public MarksDao marksDao(){
+//        return new InternalMarksDao();
+//    }
+
 }
--- Spring-education/spring-di-firstLesson/src/main/java/eu/codeacademy/service/GradeService.java
+++ Spring-education/spring-di-firstLesson/src/main/java/eu/codeacademy/service/GradeService.java
@@ -1,13 +1,17 @@
 package eu.codeacademy.service;
 
 import eu.codeacademy.dao.MarksDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+
 @Service
 public class GradeService {
-    private MarksDao marksDao;
-    public GradeService(MarksDao marksDao) {
+
+    private final MarksDao marksDao;
+
+    public GradeService( MarksDao marksDao) {
         this.marksDao = marksDao;
     }
 
