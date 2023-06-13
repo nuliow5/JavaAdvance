7d48c0168a3e799af4155e268295054d252320dc
nuliows <nuliows@gmail.com>
2023-06-13 22:37:50 +0300

    new project - social media

---
+++ social-media-REST-API/src/test/java/lt/codeacademy/socialmediaRESTAPI/SocialMediaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.socialmediaRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
